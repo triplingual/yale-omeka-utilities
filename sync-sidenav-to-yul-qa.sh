7064423c414c9e99ff7f9d2aa08f580b98e54a1d
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2026-04-24 15:33:12 -0400

    Cleaned up comments for accuracy 🛠️🗣️

--- sync-sidenav-to-yul-qa.sh
+++ sync-sidenav-to-yul-qa.sh
@@ -1,11 +1,11 @@
 echo
-echo "Dry run of syncing local version of Side Nav Omeka theme with Triptronix"
+echo "Dry run of syncing local version of Side Nav Omeka theme with the Yale Omeka.net-based QA server"
 echo "Note this does not check which git branch is checked out locally"
 echo
 rsync -auvn --delete --exclude .DS_Store --exclude README.md --exclude .git /Users/trip/Coding/omekaverse/Exhibits_2019_Theme_SideNav onlineexhibitions-qa:~
 
 echo
-echo "These are the files that will be copied to or deleted from Triptronix"
+echo "These are the files that will be copied to or deleted from the Omeka.net server"
 read -p "Proceed? " resp1
 case $resp1 in
 	[yY]|'' ) continue;;
